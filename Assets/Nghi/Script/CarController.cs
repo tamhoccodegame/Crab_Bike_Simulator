fc8cdede46fc06eed905aac682647d83d9d23b60
TranVuPhuongNghi <169137983+TranVuPhuongNghi@users.noreply.github.com>
2025-01-18 22:53:32 +0700

    Convenient Store Model

--- Assets/Nghi/Script/CarController.cs
+++ Assets/Nghi/Script/CarController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,25 +9,40 @@ public class CarController : MonoBehaviour
 
     private float horizontalInput;
     private float verticalInput;
-    private float steerAngle;
     private bool isBreaking;
+    private float steerAngle;
+
+    public float motorForce = 1000f;
+    public float brakeForce = 3000f;
+    public float maxSteeringAngle = 30f;
 
     public WheelCollider frontLeftWheelCollider;
     public WheelCollider frontRightWheelCollider;
     public WheelCollider rearLeftWheelCollider;
     public WheelCollider rearRightWheelCollider;
+
     public Transform frontLeftWheelTransform;
     public Transform frontRightWheelTransform;
     public Transform rearLeftWheelTransform;
     public Transform rearRightWheelTransform;
 
-    public float maxSteeringAngle = 30f;
-    public float motorForce = 50f;
-    public float brakeForce = 0f;
+    private Rigidbody rb;
 
-
-    private void FixedUpdate()
+    private void Start()
     {
+        // Lấy Rigidbody và hạ trọng tâm để xe ổn định hơn
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, -0.5f, 0); // Hạ trọng tâm xuống dưới trục xe
+    }
+
+    private void Update()
+    {
+        frontLeftWheelCollider.motorTorque = 0;
+        frontRightWheelCollider.motorTorque = 0;
+
+        rearLeftWheelCollider.steerAngle = 0;
+        rearRightWheelCollider.steerAngle = 0;
+
         GetInput();
         HandleMotor();
         HandleSteering();
@@ -41,33 +56,37 @@ public class CarController : MonoBehaviour
         isBreaking = Input.GetKey(KeyCode.Space);
     }
 
-    private void HandleSteering()
-    {
-        steerAngle = maxSteeringAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = steerAngle;
-        frontRightWheelCollider.steerAngle = steerAngle;
-        rearLeftWheelCollider.steerAngle = steerAngle;
-        rearRightWheelCollider.steerAngle = steerAngle;
-    }
-
-    //private void HandleMotor()
-    //{
-    //    frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-    //    frontRightWheelCollider.motorTorque = verticalInput * motorForce;
-
-    //    brakeForce = isBreaking ? 3000f : 0f;
-    //    frontLeftWheelCollider.brakeTorque = brakeForce;
-    //    frontRightWheelCollider.brakeTorque = brakeForce;
-    //    rearLeftWheelCollider.brakeTorque = brakeForce;
-    //    rearRightWheelCollider.brakeTorque = brakeForce;
-    //}
-
     private void HandleMotor()
     {
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        // Chỉ áp dụng lực kéo cho bánh sau
+        rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        // Xử lý phanh
         currentbreakForce = isBreaking ? brakeForce : 0f;
         ApplyBreaking();
+
+        //frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        //frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        //currentbreakForce = isBreaking ? brakeForce : 0f;
+        //ApplyBreaking();
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        //rearLeftWheelCollider.steerAngle = currentSteerAngle;
+        //rearRightWheelCollider.steerAngle = currentSteerAngle;
     }
 
     private void UpdateWheels()
@@ -78,14 +97,18 @@ public class CarController : MonoBehaviour
         UpdateWheelPos(rearRightWheelCollider, rearRightWheelTransform);
     }
 
-    private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
+    private void UpdateWheelPos(WheelCollider wheelCollider, Transform wheelTransform)
     {
         Vector3 pos;
         Quaternion rot;
         wheelCollider.GetWorldPose(out pos, out rot);
-        trans.rotation = rot;
-        trans.position = pos;
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
+
+
+
+
     //private float horizontalInput, verticalInput;
     //private float currentSteerAngle, currentbreakForce;
     //private bool isBreaking;
@@ -128,14 +151,6 @@ public class CarController : MonoBehaviour
     //    currentbreakForce = isBreaking ? breakForce : 0f;
     //    ApplyBreaking();
     //}
-
-    private void ApplyBreaking()
-    {
-        frontRightWheelCollider.brakeTorque = currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearRightWheelCollider.brakeTorque = currentbreakForce;
-    }
 
     //private void HandleSteering()
     //{
